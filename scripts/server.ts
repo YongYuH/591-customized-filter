038ee46f6a848f99896a03e45591ea3c742da864
YongYuH <larry830326@hotmail.com>
2021-07-18 15:58:34 +0800

    fix: update implementation of relative path in scripts

--- scripts/server.ts
+++ scripts/server.ts
@@ -7,7 +7,7 @@ const port = process?.env?.port ?? 3000
 const Fastify = fastify()
 
 Fastify.register(fastifyStatic, {
-  root: path.join(__dirname, '..', 'dist'),
+  root: path.resolve(__dirname, '..', 'dist'),
 })
 
 Fastify.listen(port, (err, address) => {
